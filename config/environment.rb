c22d68d04f4a79e92b8f0265b6fe68746d56e869
Marcin Michalowski <marcin.mi@gmail.com>
2009-10-18 14:50:32 +0200

    Installed haml and created first view

--- config/environment.rb
+++ config/environment.rb
@@ -19,6 +19,7 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
+  config.gem "haml"
   config.gem "rspec", :lib => false, :version => ">= 1.2.0"
   config.gem "rspec-rails", :lib => false, :version => ">= 1.2.0"
   config.gem 'thoughtbot-shoulda', :lib => false, :source => 'http://gems.github.com'
