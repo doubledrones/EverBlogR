a8d5acfff6e5456176f3f3e44ec7548110356bc3
Marcin Michalowski <marcin.mi@gmail.com>
2009-10-18 14:40:38 +0200

    Created notebooks controller

---
+++ spec/controllers/notebooks_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe NotebooksController do
+
+  #Delete this example and add some real ones
+  it "should use NotebooksController" do
+    controller.should be_an_instance_of(NotebooksController)
+  end
+
+end
