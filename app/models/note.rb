9b474187a992687c53306ff7885a7effe2e65e32
Marcin Michalowski <marcin.mi@gmail.com>
2009-10-28 18:04:08 +0100

    Privet methods. Proper ruby names. Method caching.

--- app/models/note.rb
+++ app/models/note.rb
@@ -12,67 +12,68 @@ require "Evernote/EDAM/limits_constants.rb"
 
 class Note
 
-  def self.userStoreTransport
-    Thrift::HTTPClientTransport.new(AppConfig.evernote['userStoreUrl'])
-  end
-
-  def self.userStoreProtocol
-    Thrift::BinaryProtocol.new(userStoreTransport)
-  end
-
-  def self.userStore
-    Evernote::EDAM::UserStore::UserStore::Client.new(userStoreProtocol)
-  end
-
-  def self.versionOK
-    userStore.checkVersion(
-      "Ruby EDAMTest",
-      Evernote::EDAM::UserStore::EDAM_VERSION_MAJOR,
-      Evernote::EDAM::UserStore::EDAM_VERSION_MINOR
-    )
-  end
-
-  def self.version_ok!
-    raise "Is my EDAM protocol version up to date?  #{versionOK}" unless versionOK
-  end
-
-  def self.authResult
-    version_ok!
-    userStore.authenticate(
-      AppConfig.evernote['username'],
-      AppConfig.evernote['password'],
-      AppConfig.evernote['consumerKey'],
-      AppConfig.evernote['consumerSecret']
-    )
-  end
-
-  def self.user
-    authResult.user
-  end
-
-  def self.noteStoreUrl
-    AppConfig.evernote['noteStoreUrlBase'] + user.shardId
-  end
-
-  def self.noteStoreTransport
-
-    Thrift::HTTPClientTransport.new(noteStoreUrl)
-  end
-
-  def self.noteStoreProtocol
-    Thrift::BinaryProtocol.new(noteStoreTransport)
-  end
-
-  def self.noteStore
-    Evernote::EDAM::NoteStore::NoteStore::Client.new(noteStoreProtocol)
-  end
-
   def self.notebook
-    noteStore.getPublicNotebook(user.id, AppConfig.evernote['public_notebooks']['marcinmisnotebook'])
+    note_store.getPublicNotebook(user.id, AppConfig.evernote['public_notebooks']['marcinmisnotebook'])
   end
 
   def self.find_all
     raise 'Write me. Please!!!'
   end
+
+  private
+
+    def self.user_store_transport
+      @@user_store_transport ||= Thrift::HTTPClientTransport.new(AppConfig.evernote['userStoreUrl'])
+    end
+
+    def self.user_store_protocol
+      @@user_store_protocol ||= Thrift::BinaryProtocol.new(user_store_transport)
+    end
+
+    def self.user_store
+      @@user_store ||= Evernote::EDAM::UserStore::UserStore::Client.new(user_store_protocol)
+    end
+
+    def self.version_ok?
+      user_store.checkVersion(
+        "Ruby EDAMTest",
+        Evernote::EDAM::UserStore::EDAM_VERSION_MAJOR,
+        Evernote::EDAM::UserStore::EDAM_VERSION_MINOR
+      )
+    end
+
+    def self.version_ok!
+      raise "Is my EDAM protocol version up to date?  #{version_ok?}" unless version_ok?
+    end
+
+    def self.auth_result
+      version_ok!
+      @@auth_result ||= user_store.authenticate(
+        AppConfig.evernote['username'],
+        AppConfig.evernote['password'],
+        AppConfig.evernote['consumerKey'],
+        AppConfig.evernote['consumerSecret']
+      )
+    end
+
+    def self.user
+      auth_result.user
+    end
+
+    def self.note_store_url
+      AppConfig.evernote['noteStoreUrlBase'] + user.shardId
+    end
+
+    def self.note_store_transport
+      @@note_store_transport ||= Thrift::HTTPClientTransport.new(note_store_url)
+    end
+
+    def self.note_store_protocol
+      @@note_store_protocol ||= Thrift::BinaryProtocol.new(note_store_transport)
+    end
+
+    def self.note_store
+      @@note_store ||= Evernote::EDAM::NoteStore::NoteStore::Client.new(note_store_protocol)
+    end
 
 end
