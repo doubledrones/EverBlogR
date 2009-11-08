70d932927ca6bc0a3231b4b712f5d04d8deb74f3
Marcin Nowicki <pr0d1r2@ragnarson.com>
2009-11-08 06:51:53 +0100

    Removed redundant spec.

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -2,10 +2,6 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Note do
 
-  it 'should return all notes from all public notebooks' do
-    pending 'Write me. Please!!!'
-  end
-
   describe 'noetbook' do
     before do
       @notebook ||= Note.notebook
